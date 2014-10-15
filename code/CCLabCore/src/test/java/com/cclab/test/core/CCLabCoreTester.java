6f141878aa1cbeed3de40d8abe609487fe5e8966
Maria Voinea <M.Voinea@student.tudelft.nl>
2014-10-15 16:57:23 +0200

    correct ignored files

--- code/CCLabCore/src/test/java/com/cclab/test/core/CCLabCoreTester.java
+++ code/CCLabCore/src/test/java/com/cclab/test/core/CCLabCoreTester.java
@@ -8,7 +8,6 @@ import java.io.IOException;
  * Created by ane on 10/15/14.
  */
 public class CCLabCoreTester {
-    //some comment to check VCS
     public static void main (String[] args) {
         try {
             ImageProcessor.createThumbnail("/Users/ane/Downloads/strawberry.jpg", "/Users/ane/Downloads/strawberry_small.jpg");
